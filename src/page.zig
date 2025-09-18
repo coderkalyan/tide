b4b9ae2a72c512fa3a95bd810967a9fe90e28f4b
Kalyan Sriram <kalyan@coderkalyan.com>
2025-09-18 09:30:44 -0700

    more experiments with page

--- src/page.zig
+++ src/page.zig
@@ -25,8 +25,41 @@ pub const PageIndex = extern struct {
 /// page, except for some extra metadata. The on-disk format is
 /// compressed with LZ4.
 pub const Page = extern struct {
-    timestamps: [max_timestamps]Timestamp,
+    /// Unique identifier for this page. Used to locate the page on disk
+    /// and build index structures.
+    ref: Ref,
+    /// Number of events stored in this page. This should be equal to the
+    /// `len` field in the corresponding `PageIndex`.
+    len: u32,
+    /// Width of each value stored in this page, in bits. This should be
+    /// equal to the `width` field in the corresponding `Signal`.
+    width: u32,
+    /// Pointer to the underlying buffer that stores the page data.
+    /// This can be extracted
+    payload: *align(page_align) const [page_size_bytes]u8,
 
-    const page_size = 16 * 1024; //< 16 KiB pages
-    const max_timestamps = page_size / @sizeOf(Timestamp);
+    const page_size_bytes = 16 * 1024; //< 16KiB footprint per page.
+    const page_align = 64; //< Cache line aligned pages.
+
+    comptime {
+        // Pages should be aligned to timestamp alignment at the least.
+        std.debug.assert(page_align >= @alignOf(Timestamp));
+        // An even number of timestamps should fit in a page.
+        std.debug.assert(page_size_bytes % @sizeOf(u64) == 0);
+    }
+
+    pub const Ref = enum(u64) { _ };
+
+    pub fn timestamps(page: *const Page) []const Timestamp {
+        const ptr: [*]const Timestamp = @ptrCast(page.payload);
+        const slice = ptr[0..page.len];
+
+        const payload <= 0;
+        std.debug.assert(&slice[slice.len - 1])
+    }
+
+    pub fn values(page: *const Page) []const u8 {
+        const ptr: [*]const u8 = page.payload + (@sizeOf(Timestamp) * page.len);
+        return ptr[0..page.len];
+    }
 };
